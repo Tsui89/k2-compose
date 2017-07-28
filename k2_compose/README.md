eea4a3fafbe1960787ac92a7a141586762d6ab4e
Cuiwenchang <756661204@qq.com>
2017-07-29 00:54:19 +0800

    initial

--- k2_compose/README.md
+++
@@ -1,7 +0,0 @@
-# k2platform deployment tool
-# Install
-```
-sudo apt-get install -y python-pip
-pip install k2-compose
-k2-compose -h
-```
--- k2_compose/common/globalVar.py
+++
@@ -1,9 +0,0 @@
-class GlobalVar:
-  debug = None
-
-
-def set_debug():
-  GlobalVar.debug = True
-
-def get_debug():
-  return GlobalVar.debug
